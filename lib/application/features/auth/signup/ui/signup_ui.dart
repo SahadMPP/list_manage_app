ea541710fc1b0a342261b1f9d02a355550f9fd74
Sahad mp <muhdsahad4916@gmail.com>
2024-07-04 09:21:44 +0530

    sign in and splash ui

---
+++ lib/application/features/auth/signup/ui/signup_ui.dart
@@ -0,0 +1,155 @@
+
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:list_app/application/core/widgets/custom_button.dart';
+import 'package:list_app/application/features/auth/widgets/custum_textfield.dart';
+
+class SignUpPage extends StatefulWidget {
+  const SignUpPage({super.key});
+
+  @override
+  State<SignUpPage> createState() => _SignUpPageState();
+}
+
+class _SignUpPageState extends State<SignUpPage> {
+  TextEditingController passwordConformationCon = TextEditingController();
+  TextEditingController namecontroller = TextEditingController();
+  TextEditingController emailcontroller = TextEditingController();
+  TextEditingController passwordcontroller = TextEditingController();
+
+  GlobalKey<FormState> fromKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(24.0),
+          child: Column(
+            children: [
+              const SizedBox(height: 50),
+              Align(
+                alignment: Alignment.topCenter,
+                child: Text(
+                  "Register Here",
+                  style: GoogleFonts.aBeeZee(
+                    textStyle: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 30,
+                        fontWeight: FontWeight.bold),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 20),
+              Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 30),
+                child: Text(
+                  "Hey,Enter your details to get sign up to your account",
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.aBeeZee(
+                    textStyle: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 14,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              Form(
+                key: fromKey,
+                child: Column(
+                  children: [
+                    CustomTextField(
+                        validatorText: 'Enter your name',
+                        controller: namecontroller,
+                        hintText: "Enter Name",
+                        isPassword: false),
+                    CustomTextField(
+                        isEmail: true,
+                        validatorText: 'Enter your email',
+                        controller: emailcontroller,
+                        hintText: "Enter Email",
+                        isPassword: false),
+                    CustomTextField(
+                        validatorText: 'Enter your password',
+                        controller: passwordcontroller,
+                        hintText: "Enter Password",
+                        isPassword: true),
+                    CustomTextField(
+                        validatorText: 'Field is empty',
+                        controller: passwordConformationCon,
+                        hintText: "Conform Password",
+                        isPassword: true),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Having trouble in sign in?',
+                  style: GoogleFonts.aBeeZee(
+                    textStyle: const TextStyle(
+                        color: Colors.black,
+                        fontSize: 10,
+                        fontWeight: FontWeight.w500),
+                  ),
+                ),
+              ),
+              const SizedBox(height: 40),
+              CustomButton(
+                  text: "Sign Up",
+                  fun: () {
+                    if (fromKey.currentState!.validate()) {
+                      if (passwordConformationCon.text !=
+                          passwordcontroller.text) {
+                        ScaffoldMessenger.of(context)
+                            .showSnackBar(const SnackBar(
+                          content: Text(
+                            "conform password does not match",
+                            style: TextStyle(color: Colors.white),
+                          ),
+                          backgroundColor: Colors.red,
+                          margin: EdgeInsets.all(8),
+                          behavior: SnackBarBehavior.floating,
+                        ));
+                        return;
+                      }
+                    }
+                  }),
+              const SizedBox(height: 100),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text(
+                    'Do you have Already an account?',
+                    style: GoogleFonts.aBeeZee(
+                      textStyle: const TextStyle(
+                          color: Colors.black45,
+                          fontSize: 10,
+                          fontWeight: FontWeight.w600),
+                    ),
+                  ),
+                  const SizedBox(width: 5),
+                  InkWell(
+                    onTap: () {},
+                    child: Text(
+                      'Sign in',
+                      style: GoogleFonts.aBeeZee(
+                        textStyle: const TextStyle(
+                            color: Colors.black,
+                            fontSize: 11,
+                            fontWeight: FontWeight.w600),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:list_app/application/features/auth/signin/ui/sign_ui.dart';
+import 'package:list_app/application/features/auth/splash/ui/splash_ui.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      home: SignInPage(),
+      home: SplashScreen(),
     );
   }
 }
